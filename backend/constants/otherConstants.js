231a56c3dc7bef813c46e759f780b69027a36e4f
Suparna A K <suparnaak@gmail.com>
2025-11-04 10:49:00 +0530

    trying with another gemini model

--- backend/constants/otherConstants.js
+++ backend/constants/otherConstants.js
@@ -8,7 +8,7 @@ const CONSTANTS = {
     },
   },
   AI: {
-    GEMINI_CHAT_MODEL: "gemini-1.5-flash",
+    GEMINI_CHAT_MODEL: "gemini-1.5-flash-001",
     GEMINI_EMBEDDING_MODEL: "text-embedding-004",
     RESUME_CHAR_LIMIT: 2000,
   },
